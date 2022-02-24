c19791c6af302c7b4f1e7619842906ee3ea504eb
Eraraya Morenzo Muten <morenzomuten@gmail.com>
2022-02-24 17:44:14 +0700

    lab 0

---
+++ lab-0-introduction/hello.c
@@ -0,0 +1,18 @@
+/** EL2208 Praktikum Pemecahan Masalah dengan C 2021/2022
+*   Modul               : 0
+*   Percobaan           : 1
+*   Hari dan Tanggal    : Rabu, 9 Februari 2021
+*   Nama (NIM)          : Eraraya Morenzo Muten (18320003)
+*   Asisten (NIM)       : Dismas Widyanto (13218065)
+*   Nama File           : hello.c
+*   Deskripsi           : Program test assignment di GitHub classroom.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello, World!");
+   
+   return 0;
+}
